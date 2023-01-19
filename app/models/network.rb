010458d305487a4efa174075800a18c230fa3efd
Samuel Adelson <adelsonllc@gail.com>
2023-01-19 09:50:33 -0500

    commit tree aa253a5a7bf77dae68fc8efef03bb3b6aeecb113

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,7 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+
+  def sorry
+    "We're sorryabout passing on John ulaney's pilot"
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+
+
+        def actor_list
+            self.actor.map do |a|
+                 "#{a.full_name}"
+        end
+    end
 end
